7758cfe1cbb1eb676511fbfdcb83c08f9f62ca45
jmartine2319 <jmartinezopg@gmail.com>
2020-10-07 23:58:42 -0500

    Primera version del aplicativo

---
+++ src/main/java/com/jdmm/quasar/ControladorQuasar.java
@@ -0,0 +1,79 @@
+package com.jdmm.quasar;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.jdmm.quasar.dto.ResultadoSatellitesOut;
+import com.jdmm.quasar.dto.ResultadoUbicacion;
+import com.jdmm.quasar.dto.SatelliteIn;
+import com.jdmm.quasar.dto.SatellitesIn;
+import com.jdmm.quasar.dto.Ubicacion;
+import com.jdmm.quasar.logica.LogicaQuasar;
+import com.jdmm.quasar.util.ExcepcionQasar;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class ControladorQuasar {
+    /**
+     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
+     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query" SatellitesIn
+     */
+    @FunctionName("quasar")
+    public HttpResponseMessage run(
+            @HttpTrigger(
+                name = "topsecret",
+                methods = {HttpMethod.POST},
+                authLevel = AuthorizationLevel.ANONYMOUS)
+                HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Se recibe peticion para ubicacion de los satellites");
+
+        // Parse query parameter
+        final String query = request.getQueryParameters().get("name");
+        final String satelliteString = request.getBody().orElse(query);
+        Gson gson = new Gson();
+        SatellitesIn satellites = gson.fromJson(satelliteString, SatellitesIn.class);
+
+        if (satelliteString == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Datos incompletos").build();
+        } else {
+        	LogicaQuasar logica = new LogicaQuasar();
+        	ResultadoUbicacion resultado;
+        	ResultadoSatellitesOut resultadoSatellite = new ResultadoSatellitesOut();
+			try {
+				resultado = logica.iniciarProcesoUbicacion(satellites);
+				
+	        	if(resultado.isExitoso()) {
+	        		context.getLogger().info("|| pasoo : " + resultado.getMensaje() );
+	        		Ubicacion ubicacion = new Ubicacion();
+	        		ubicacion.setX(resultado.getPosicionX());ubicacion.setY(resultado.getPosicionY());
+	        		resultadoSatellite.setMessage(resultado.getMensaje());
+	        		resultadoSatellite.setPosition(ubicacion);
+	        		return request.createResponseBuilder(HttpStatus.OK).body(resultadoSatellite).build();
+	        	}else {
+	        		context.getLogger().severe("Ocurrio un error inesperado");
+	        		return request.createResponseBuilder(HttpStatus.NOT_FOUND).body(resultadoSatellite).build();
+	        	}
+			} catch (ExcepcionQasar e) {
+				context.getLogger().severe(e.getMessage());
+				return request.createResponseBuilder(HttpStatus.NOT_FOUND).body(resultadoSatellite).build();
+			}
+        	
+            
+        }
+    }
+}
---
+++ src/main/java/com/jdmm/quasar/logica/LogicaQuasar.java
@@ -0,0 +1,165 @@
+package com.jdmm.quasar.logica;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.math3.fitting.leastsquares.LevenbergMarquardtOptimizer;
+import org.apache.commons.math3.fitting.leastsquares.LeastSquaresOptimizer.Optimum;
+
+import com.jdmm.quasar.dto.ResultadoUbicacion;
+import com.jdmm.quasar.dto.SatellitesIn;
+import com.jdmm.quasar.util.ExcepcionQasar;
+import com.jdmm.quasar.util.RedisClient;
+import com.lemmingapex.trilateration.NonLinearLeastSquaresSolver;
+import com.lemmingapex.trilateration.TrilaterationFunction;
+
+public class LogicaQuasar {
+	
+	public ResultadoUbicacion iniciarProcesoUbicacion(SatellitesIn satellites) throws ExcepcionQasar {
+		ResultadoUbicacion resultado = new ResultadoUbicacion();
+		resultado.setExitoso(false);
+		List<LinkedHashMap<Integer,String>> listaMensajes = new ArrayList<>();
+		double[] listaDistancias = new double[3];
+		if(satellites.getSatellites().size()>3) {
+			return resultado;
+		}
+		for(int i=0;i<satellites.getSatellites().size();i++) {
+			LinkedHashMap<Integer,String> mensajes=limpiarMensaje(satellites.getSatellites().get(i).getMessage());
+			listaMensajes.add(mensajes);
+			listaDistancias[i]=satellites.getSatellites().get(i).getDistance();
+		}
+		ResultadoUbicacion mensaje = fusionarMensajes(listaMensajes);
+		ResultadoUbicacion distancia = calcularUbicacion(listaDistancias);
+		if(mensaje.isExitoso() && distancia.isExitoso()) {
+			resultado.setExitoso(true);
+			resultado.setPosicionX(distancia.getPosicionX());
+			resultado.setPosicionY(distancia.getPosicionY());
+			resultado.setMensaje(mensaje.getMensaje());
+		}else {
+			if(!mensaje.isExitoso()) {
+				throw new ExcepcionQasar("Ocurrio un error al descifrar los mensajes");
+			}
+			if(!distancia.isExitoso()) {
+				throw new ExcepcionQasar("Ocurrio un error al calcular la ubicacion de los satellites");
+			}
+		}
+		return resultado;
+	}
+
+	/**
+	 * Metodo para calcular la ubicacion usando la distancia respecto a los otros satelites
+	 */
+	private ResultadoUbicacion calcularUbicacion(double[] listaDistancias) {
+		ResultadoUbicacion resultado = new ResultadoUbicacion();
+		resultado.setExitoso(true);
+		try {
+			double[] kenobi = obtenerUbicacion("kenobi");
+			double[] skywalker = obtenerUbicacion("skywalker");
+			double[] sato = obtenerUbicacion("sato");
+			if(kenobi.length==0 && skywalker.length==0 && sato.length==0 ) {
+				System.out.println("no se pudo obtener las propiedades");
+				resultado.setExitoso(false);
+				return resultado;
+			}
+			double[][] posiciones = new double[][] { kenobi, skywalker, sato };
+	
+			NonLinearLeastSquaresSolver solver = new NonLinearLeastSquaresSolver(new TrilaterationFunction(posiciones, listaDistancias), new LevenbergMarquardtOptimizer());
+			Optimum optimum = solver.solve();
+	
+			double[] centroid = optimum.getPoint().toArray();
+	
+			resultado.setPosicionX(centroid[0]);
+			resultado.setPosicionY(centroid[1]);
+		}catch(Exception e) {
+			resultado.setExitoso(false);
+		}
+		return resultado;
+	}
+	
+	private double[] obtenerUbicacion(String nombreSatellite) {
+		RedisClient redis = new RedisClient();
+		String propiedad = redis.consultarPropiedad(nombreSatellite);
+		String[] ubicacionXY=propiedad.split(",");
+		double[] ubicacionSatellite = new double[2];
+		if(propiedad.equals("") || ubicacionXY.length<2) {
+			return ubicacionSatellite;
+		}
+		ubicacionSatellite[0]=Double.parseDouble(ubicacionXY[0]);
+		ubicacionSatellite[0]=Double.parseDouble(ubicacionXY[1]);
+		return ubicacionSatellite;
+	}
+	
+	/**
+	 * Metodo para verificar que no llegue datos que no sean basura
+	 * @param mensajes
+	 * @return
+	 */
+	private static LinkedHashMap<Integer,String> limpiarMensaje(List<String> mensajes) {
+	
+		LinkedHashMap<Integer,String> mensajeDes = new LinkedHashMap<Integer,String>();
+		for(int i=0;i<mensajes.size();i++) {
+			
+			String pattern = "[^A-Za-z0-9]";
+			Pattern r = Pattern.compile(pattern);
+			Matcher m = r.matcher(mensajes.get(i));
+		    if (m.find( )) {
+		    	mensajeDes.put(i,"");
+		    }else {
+		    	mensajeDes.put(i, mensajes.get(i));
+		    }
+			
+		}
+		return mensajeDes;
+	}
+	
+	/**
+	 * Metodo que fusiona los mensajes recibidos por los satellites
+	 * @param mensajes
+	 * @return
+	 */
+	private ResultadoUbicacion fusionarMensajes(List<LinkedHashMap<Integer,String>> mensajes) {
+		ResultadoUbicacion mensajeOut = new ResultadoUbicacion();
+		mensajeOut.setExitoso(true);
+		LinkedHashMap<Integer,String> mensajeDescifrado = new LinkedHashMap<Integer,String>();
+		int tamanoArreglo=0;
+		for(int i =0;i<mensajes.size();i++) {
+			tamanoArreglo = mensajes.size();
+			if(i>0 && tamanoArreglo>0) {
+				if(tamanoArreglo>mensajes.size() || tamanoArreglo<mensajes.size()) {
+					mensajeOut.setExitoso(false);
+					return mensajeOut;
+				}
+			}
+			LinkedHashMap<Integer,String> mensaje= mensajes.get(i);
+			if(i==0) {
+				mensaje.forEach((key,value) -> {
+					mensajeDescifrado.put(key, value);
+				});
+			}else {
+				mensaje.forEach((key,value) -> {
+					if(value.length()>0) {
+						mensajeDescifrado.put(key, value);
+					}
+					
+				});
+			}
+		}
+		StringBuffer mensajeDes = new StringBuffer();
+		mensajeDescifrado.forEach((key,value) -> {
+			
+			if(key==0) {
+				mensajeDes.append(value);
+			}else {
+				mensajeDes.append(" ");
+				mensajeDes.append(value);
+			}
+			
+			
+		});
+		mensajeOut.setMensaje(mensajeDes.toString());
+		return mensajeOut;
+	}
+}
